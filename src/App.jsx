a354bf8d3f7d3d829b9be42ccc9ece1bdc8a4d0a
NouranAlSharawneh <noransharawna@hotmail.com>
2024-04-09 13:46:05 +0300

    added the grid and the hero title

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
 
@@ -5,8 +6,46 @@ function App() {
   return (
     <section>
       <NavBar />
+      <HeroSection />
     </section>
   );
 }
 
 export default App;
+
+function HeroSection() {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (textRef.current) {
+      const textNode = textRef.current;
+      const text = textNode.textContent;
+      const modifiedText = text
+        .split("")
+        .map(
+          (char, i) => `<span 
+        style="transform:rotate(${i * 9}deg)">${char}</span>`
+        )
+        .join("");
+      textNode.innerHTML = modifiedText;
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="title__text">anime quote generator</div>
+      <div className="side__image">image</div>
+      <div className="main__image">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
+        praesentium veritatis eum non, corrupti at id cupiditate eveniet
+        excepturi molestias. Laborum deleniti magni illum dignissimos quo
+        cupiditate ratione fugit eveniet.
+      </div>
+      <div className="rounded__text">
+        <div className="inner__image"></div>
+        <p ref={textRef}>- Anime Quote Generator - Platform - </p>
+      </div>
+      <div className="quote__text">quote text</div>
+    </div>
+  );
+}
